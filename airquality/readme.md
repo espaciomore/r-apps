d35e2a54534480f9486bb8e2d24c44929cd2f975
Manuel Cerda <espaciomore@gmail.com>
2018-01-21 03:00:39 -0400

    first commit

---
+++ airquality/readme.md
@@ -0,0 +1,37 @@
+New York City's Air Quality
+========================================================
+author: Manuel Cerda
+date: January 21st, 2018
+autosize: true
+
+
+
+Description
+========================================================
+
+Daily air quality measurements in New York, May to September 1973.
+
+A data frame with 154 observations on 6 variables.
+
+1.	Ozone (ppb)
+2.	Solar.R (lang)
+3.	Wind (mph)
+4.	Temp (F)
+5.	Month (1:12)
+6.	Day (1:31)
+
+
+Instructions
+========================================================
+
+New York City's daily temperature during the summer time.
+
+Because the observations are available only from June to September, you will be able to select the temperature per month.
+
+Sorrounding the temperature is the feature; Ozone, Solar R. and Wind.
+
+
+
+========================================================
+
+<iframe width="900" height="800" frameborder="0" scrolling="no" src="https://mcerda.shinyapps.io/nyc_airquality/"></iframe>
---
+++ airquality/server.R
@@ -0,0 +1,38 @@
+library(shiny)
+
+# define server logic for rendering the plot
+shinyServer(function(input, output){
+  output$aq_plot <- renderPlot({
+	# load data from R built-in data set
+	data("airquality")
+	# create a Linear Model
+	fit.data <- airquality[airquality$Month == input$month,]
+	fit.lm <- lm(
+	    formula=Temp ~ Day, # I would have included 'Ozone+Solar.R+Wind' only for prediction 
+	    data=fit.data, 
+	    na.action=na.exclude
+	) 
+
+	# plot the Temp per Day
+	plot(x=fit.data$Day, y=fit.data$Temp, 
+		xlim=c(0,31), ylim=c(50,110), 
+		xlab="Day", ylab="Temperature (Farenheit)", 
+		col="white", pch='.', axes=T, bty="n")
+	title(main="New York City's Temperature by Day")
+	# add the dependent variable Ozone
+	points(x=fit.data$Day, y=fit.data$Temp, cex = (fit.data$Ozone/max(fit.data$Ozone, na.rm=T))*10, col = "yellow")
+	# add the dependent variable Solar.R
+	points(x=fit.data$Day, y=fit.data$Temp, cex = (fit.data$Solar.R/max(fit.data$Solar.R, na.rm=T))*10, col = "orange")
+	# add the dependent variable Wind
+	points(x=fit.data$Day, y=fit.data$Temp, cex = (fit.data$Wind/max(fit.data$Wind, na.rm=T))*10, col = "light blue")
+	# add the regression line
+	abline(fit.lm, col="green", lty=2)
+	# add legend for dependent variables
+	legend(-1, 115, legend=c("Ozone", "Solar.R", "Wind"), 
+		col = c("yellow", "orange", "light blue"),
+    	text.col = "gray", lty = c(1, 1, 1), box.lty=0,
+    	bg="transparent"
+ 	)
+
+  })
+})
