105e7bb2acf6a78e29fc5c3ba2353eedfcaa832d
Manuel Cerda <espaciomore@gmail.com>
2018-03-17 16:09:03 -0400

    remove unncesessary files

--- capstone/ui.R
+++ capstone/ui.R
@@ -9,9 +9,10 @@ shinyUI(
 	  ),
 	  mainPanel(
 	    p("This is a Shiny App designed for Natural Language Processing, Its algorithm builds n-grams with text from Tweets, News and Blogs."),
+	    p(),
 	    br(),
-	    h4("How to use?"),
-	    p(textOutput("bestmatch"), "User types the text in the search box on the left panel then wait for the next word suggestions to show below.")
+	    h4("How to use It?"),
+	    p(textOutput("bestmatch"), "User types the text in the search box on the left panel then waits for the next word suggestions to show below.")
 	  ),
 		title = "Coursera - NLP Capstone Project - Next Word Prediction"
 	)
